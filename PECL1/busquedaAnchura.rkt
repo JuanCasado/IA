67f1f9ca9b81ea50b6075e3421705b424b153b2e
Juan Casado Ballesteros <juan.casado@edu.uah.es>
2019-03-31 17:36:32 +0200

    busqueda anchura

--- PECL1/busquedaAnchura.rkt
+++ PECL1/busquedaAnchura.rkt
@@ -1,2 +1,5 @@
 #lang racket
 
+(define (insertar_siguientes_anchura siguientes abiertos)
+  (if (null? siguientes) abiertos
+      (insertar_siguientes_anchura (cdr siguientes) (reverse (cons (car siguientes) (reverse abiertos))))))
